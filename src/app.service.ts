6d026cf7ecfd54c985ac428d116867672364dad8
maykhitarian <mayconfelipe164@gmail.com>
2022-08-27 21:32:57 -0300

    add: PrismaService

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  constructor(private prisma: PrismaService) { }
+
+  async getHello(): Promise<any> {
+    const users = await this.prisma.user.findMany();
+    return users;
   }
 }
